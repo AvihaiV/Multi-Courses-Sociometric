cb771566fcae21a77bd9d727b5c95d38c8dc2481
Avihai <avihai673@gmail.com>
2018-06-10 14:59:07 +0300

    Uploading the final version of the project.

--- MultiCoursesSociometric.js
+++ MultiCoursesSociometric.js
@@ -1,7 +1,7 @@
-// Host of your Neo4j installation
+// Host of Neo4j installation
 const NEO4J_HOST = 'bolt://localhost:7687';
 
-// Credentails of your Neo4j
+// Credentails of  Neo4j
 const NEO4J_USERNAME="neo4j"
 const NEO4J_PASSWORD="neo4j1"
 
@@ -11,57 +11,203 @@ const driver = neo4j.v1.driver(NEO4J_HOST, neo4j.v1.auth.basic(NEO4J_USERNAME, N
 const session = driver.session();
 
 
-// Create a function to get all soldiers count
+// a function to get all test soldiers count
 function getSoldiersCount(){
-	var query = 'MATCH (sold:Soldier) return count(sold) as count';
+	var query = 'MATCH (sold:TestSoldier) return count(sold) as count';
 	session.run(query).then( function (result) {
 		// Iterate over all the records
 		result.records.forEach(function (record) {
-			//console.log( record.get('name'), ":", record.get('screen_name') );
-			console.log('Total Soldiers:',record.get('count').low);
+
+			//  Change the SoldierCount on UI
+			$("#SoldierCount").html('מספר החיילים: '+record.get('count').low).css('direction','rtl');
+
 		});
-		// Close the neo4j session
-		//session.close();
 	}).catch(function (error) {
 		console.log(error);
 	});
 }
 
-//Function to add new soldier to neo4j
+//a function to get all input soldiers count
+function OpGetSoldiersCount(){
+	var query = 'MATCH (sold:Soldier) return count(sold) as count';
+	session.run(query).then( function (result) {
+		// Iterate over all the records
+		result.records.forEach(function (record) {
+
+			//  Change the SoldierCount on UI
+			$("#OpSoldierCount").html('מספר החיילים: '+record.get('count').low).css('direction','rtl');
+
+		});
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+// a function to get all test course count
+function getCourseCount(){
+	var query = 'MATCH (course:TestCourse) return count(course) as count';
+	session.run(query).then( function (result) {
+		// Iterate over all the records
+		result.records.forEach(function (record) {
+
+			//  Change the CourseCount on UI
+			$("#CourseCount").html('<span>מספר הקורסים: </span> <span>'+record.get('count').low+'</span>').css('direction','rtl');
+		
+		});
+
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+//a function to get all input course count
+function OpGetCourseCount(){
+	var query = 'MATCH (course:Course) return count(course) as count';
+	session.run(query).then( function (result) {
+		// Iterate over all the records
+		result.records.forEach(function (record) {
+
+			//  Change the CourseCount on UI
+			$("#OpCourseCount").html('<span>מספר הקורסים: </span> <span>'+record.get('count').low+'</span>').css('direction','rtl');
+		
+		});
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+// A function to get all test courses
+function getAllCourses(){
+	var query = 'MATCH (course:TestCourse) return course.id as course_id ORDER BY course_id';
+	session.run(query).then( function (result) {
+
+		// Empty previous table
+		$("#courseList").html('');
+		$("#courseList").append('<a href="#" class="list-group-item list-group-item-success">מספר קורס</a>');
+
+		// Iterate over all the records
+		result.records.forEach(function (record) {
+			//console.log('Course Id:',record.get('course_id'));
+			$("#courseList").append('  <a class="list-group-item list-group-item-info">'+record.get('course_id')+'</a>');
+		});
+
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+//A function to get all input courses
+function OpGetAllCourses(){
+	var query = 'MATCH (course:Course) return course.id as course_id ORDER BY course_id';
+	session.run(query).then( function (result) {
+
+		// Empty previous table
+		$("#OpCourseList").html('');
+		$("#OpCourseList").append('<a href="#" class="list-group-item list-group-item-success">מספר קורס</a>');
+
+		// Iterate over all the records
+		result.records.forEach(function (record) {
+			//console.log('Course Id:',record.get('course_id'));
+			$("#OpCourseList").append('  <a class="list-group-item list-group-item-info">'+record.get('course_id')+'</a>');
+		});
+
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+// A function to get all test soldiers
+function getAllSoldiers(){
+	var query = 'MATCH (soldier:TestSoldier) return toInt(soldier.id) as soldier_id ORDER BY soldier_id';
+	session.run(query).then( function (result) {
+
+		// Empty previous table
+		document.getElementById("soldierList").innerHTML="";
+		$("#soldierList").append('<a href="#" class="list-group-item list-group-item-success">מספר אישי</a>');
+
+		// Iterate over all the records
+		result.records.forEach(function (record) {
+			//console.log('Course Id:',record.get('course_id'));
+			$("#soldierList").append('  <a class="list-group-item list-group-item-info">'+record.get('soldier_id')+'</a>');
+		});
+
+
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+//A function to get all input soldiers
+function OpGetAllSoldiers(){
+	var query = 'MATCH (soldier:Soldier) return toInt(soldier.id) as soldier_id ORDER BY soldier_id';
+	session.run(query).then( function (result) {
+
+		// Empty previous table
+		//document.getElementById("OpSoldierList").innerHTML="";
+		$("#OpSoldierList").html('');
+		$("#OpSoldierList").append('<a href="#" class="list-group-item list-group-item-success">מספר אישי</a>');
+
+		// Iterate over all the records
+		result.records.forEach(function (record) {
+			//console.log('Course Id:',record.get('course_id'));
+			$("#OpSoldierList").append('  <a class="list-group-item list-group-item-info">'+record.get('soldier_id')+'</a>');
+		});
+
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+// Function to add new soldier to neo4j
 function addSoldier(soldier){
 	// Construct a Neo4j Query to add soldier (If soldier not exists else merge the details)
-	var query = 'MERGE (soldier:Soldier {name : {name} }) RETURN soldier.name AS name';
+	var query = 'MERGE (soldier:Soldier {id : {id} }) RETURN soldier.id AS id';
 
 	// Run query using session
 	session.run(query,soldier).then( function (result) {
 		// log success message
 		console.log("Success")
-
-		// Get soldier count
-		getSoldiersCount();
-
-		// Close the neo4j session
-		//session.close();
 	}).catch(function (error) {
 		console.log(error);
 	});
 }
 
+//Function to add new Course to neo4j
+function addCourse(course){
+	// Construct a Neo4j Query to add Course (If Course not exists else merge the details)
+	var query = 'MERGE (course:Course {id : {id} })';
+
+	// Run query using session
+	session.run(query,course).then( function (result) {
+		// log success message
+		console.log("Success")
+
+	}).catch(function (error) {
+		console.log(error);
+	});
+
+}
+
 // Function to add grade for soldier to neo4j
 function addGradeToNeo4j(grades){
 	// Add grade giving soldier if not exists
-	var soldier={'name':grades.gradeFrom}
+	var soldier={'id':grades.gradeFrom}
 	addSoldier(soldier);
 
 	// Construct a Neo4j Query to add grades to a soldier
-	//var query = 'MATCH (from:Soldier),(to:Soldier) WHERE from.name = {gradeFrom} AND to.name = {gradeTo} CREATE (from)-[r:GRADED { course: {course},grade:{grade} }]->(to)'
-	//var query = 'MATCH (from:Soldier)-[]->(to:Soldier) WHERE from.name = {gradeFrom} AND to.name = {gradeTo} CREATE (from)-[r:GRADED { course: {course},grade:{grade} }]->(to)'
-	var query = 'MATCH (from:Soldier),(to:Soldier) WHERE from.name = {gradeFrom} AND to.name = {gradeTo} MERGE (from)-[r:GRADED { course: {course}}]->(to) SET r.grade={grade}';
+	var query = 'MATCH (from:Soldier),(to:Soldier),(course:Course) '
+		+ 'WHERE from.id = {gradeFrom} AND to.id = {gradeTo} AND course.id = {course} '
+		+ 'MERGE (grade:Grade {id:"'+ grades.gradeFrom+'_'+grades.gradeTo+'_'+grades.course+'",course:'+grades.course+'}) '
+		+ 'MERGE (from)-[given:GIVEN]->(grade) '
+		+ 'MERGE (to)-[received:RECEIVED]->(grade) '
+		+ 'MERGE (grade)-[for:FOR]->(course) '
+		+ 'MERGE (to)-[opted:OPTED]->(course) '
+		+ 'SET grade.grade={grade}' ;
 
 	// Run query using session
 	session.run(query,grades).then( function (result) {
-		// Close the neo4j session
-		//session.close();
+
+
 	}).catch(function (error) {
 		console.log(error);
 	});
@@ -72,21 +218,24 @@ function getSoldierData(filters){
 	var response=[];
 
 	// Construct a Neo4j Query to get grades of a soldier
-	var query = 'MATCH (s1:Soldier{'
-	+ (""!=filters.name?'name:{name}':'')
-	+ '})<-[g:GRADED{'
-	+ ((""!=filters.grade && false==isNaN(filters.grade))?'grade:{grade}':'')
-	+ ((""!=filters.grade && ""!=filters.course && false==isNaN(filters.grade))? ',':'')
-	+ (""!=filters.course?'course:{course}':'')
-	+ '}]-(s2:Soldier) return s1.name as name,s2.name as graded_by, g.grade as grade, g.course as course ORDER BY name, grade DESC';
+	var query = 'MATCH (s1:Soldier{' 
+	+ ((false==isNaN(filters.id))?'id:{id}':'') 
+	+ '})-[:RECEIVED]->(g:Grade{'
+	+ ((false==isNaN(filters.grade))?'grade:{grade}':'')
+	+ ((false==isNaN(filters.grade) && false==isNaN(filters.course)) ? ',':'')
+	+ ((false==isNaN(filters.course))?'course:{course}':'') +'}), '
+	+ '(g)<-[:GIVEN]-(s2:Soldier) '
+	+ ' return s1.id as id,s2.id as graded_by, g.grade as grade, g.course as course ORDER BY id, grade DESC';
 
+	console.log("q:"+query);
+	
 	// Run query using session
 	session.run(query,filters).then( function (result) {
-		// Close the neo4j session
+
 		result.records.forEach(function (record) {
 			//console.log( record.get('name'), ":", record.get('grade'),":", record.get('course') );
 			var obj={};
-			obj['name']=record.get('name')
+			obj['name']=record.get('id')
 			obj['graded_by']=record.get('graded_by')
 			obj['course']=record.get('course')
 			obj['grade']=record.get('grade')
@@ -105,7 +254,7 @@ function getSoldierData(filters){
 			// Please write your own alert here.
 			alert('No data available for the applied filters!');
 		}
-		console.log("SUCCESS");
+
 	}).catch(function (error) {
 		console.log(error);
 	});
@@ -115,10 +264,10 @@ function getSoldierData(filters){
 function drawTable(data) {
 	// Define Header for table graded_by
 	header={
-		'name':'Soldier ID',
-		'graded_by':'Graded By',
-		'course':'Course',
-		'grade':'Grade'
+		'name':'מספר אישי',
+		'graded_by':'קיבל ציון מ',
+		'course':'קורס',
+		'grade':'ציון'
 	}
 	// Call a function to draw row
 	drawRow(header)
@@ -150,20 +299,30 @@ var calc=0;
 
 function inputData()
 {
-	var perNum =document.getElementById("personalNum").value;
-	var courNum=document.getElementById("courNum").value;
+	var perNum = parseInt(document.getElementById("personalNum").value);
+	var courNum = parseInt(document.getElementById("courNum").value);
 	// Create soldier object
-	var soldier={
-		'name':perNum,
-	}
 	// Call funcction to add soldier
-	addSoldier(soldier);
+	if("" != perNum){
+		var soldier={
+				'id':perNum,
+			}
+		addSoldier(soldier);
+	}
+	if(!isNaN(courNum)){
+		var course={
+			'id':courNum
+		}
+		addCourse(course);
+	}
+	OpUpdateStatistics();
+
 }
 
 function addGrade(){
 
-    pNumbers[i]=document.getElementById("perNum").value;
-    grades[i]=parseInt(document.getElementById("grade").value);
+    pNumbers[i] = parseInt(document.getElementById("perNum").value);
+    grades[i] = parseInt(document.getElementById("grade").value);
     sum+=grades[i];
 	// Soldier who is giving grade
 	var from = pNumbers[i];
@@ -174,9 +333,9 @@ function addGrade(){
     //alert(sum);
 
 	// Soldier to whom grade is given
-	var to = document.getElementById("personalNum").value;
+	var to = parseInt(document.getElementById("personalNum").value);
 	// the course for which this grade is for
-	var courNum=document.getElementById("courNum").value;
+	var courNum=parseInt(document.getElementById("courNum").value);
 
 	// create grades object
 	var grade={
@@ -186,32 +345,21 @@ function addGrade(){
 		'grade':grade
 	}
 	addGradeToNeo4j(grade);
-}
-
-
-function newGrade()
-{
+	OpUpdateStatistics();
     document.getElementById("perNum").value="";
     document.getElementById("grade").value="";
-}
-
-function calcGrade()
-{
-    calc= sum/i;
-    var text;
-    text=   "הציון המשוקלל הוא:  " + calc;
-    document.getElementById("calculation").innerHTML = text;
 
 }
+
 
 function outputData() {
-	var personalNum = document.getElementById("perNumOut").value;
-	var courNum = document.getElementById("courNumOut").value;
+	var personalNum = parseInt(document.getElementById("perNumOut").value);
+	var courNum = parseInt(document.getElementById("courNumOut").value);
 	var grade =  parseInt(document.getElementById("mySelect").value)
     //alert ("hello")
 	// create filters object
 	var filters={
-		'name':personalNum,
+		'id':personalNum,
 		'course':courNum,
 		'grade':grade
 	}
@@ -219,6 +367,128 @@ function outputData() {
 	getSoldierData(filters);
 
 }
+
+function getAvgScoreForSoldier(page){
+
+	console.log(page);
+	if('output'==page){
+		var soldierId = document.getElementById("OpSoldierIdForAvgCourse").value;
+		var html_element="OpCourseAvgForSoldier";
+		var nodeLabel="";
+		var relLabel="";
+	}else if ('stats'==page){
+		var soldierId = document.getElementById("soldierIdForAvgCourse").value;
+		var html_element="courseAvgForSoldier";
+		var nodeLabel="Test";
+		var relLabel="TEST_";
+	}
+	
+	// Call function to display output
+	if(null == soldierId  || '' == soldierId){
+		alert('אנא הכנס מספר אישי!');
+
+	} else {
+		getSoldierDataForCourses(soldierId,html_element,nodeLabel,relLabel);
+	}
+
+}
+
+function getCourseAvgForSoldier(page){
+	console.log(page);
+
+	if('output'==page){
+		var courseId = document.getElementById("OpCourseIdForAvg").value;
+		var html_element="OpAvgForCourse";
+		var nodeLabel="";
+		var relLabel="";
+	}else if ('stats'==page){
+		var courseId = document.getElementById("courseIdForAvg").value;
+		var html_element="avgForCourse";
+		var nodeLabel="Test";
+		var relLabel="TEST_";
+
+	}
+
+	// Call function to display output
+	if('' == courseId || null == courseId){
+		alert('אנא הכנס מספר קורס!');
+
+	} else {
+		getCourseDataForSoldiers(courseId,html_element,nodeLabel,relLabel);
+	}
+}
+
+
+function updateStatistics(){
+
+	// Get All Soldiers Count
+	getCourseCount();
+
+	// Get All Soldiers Count
+	getSoldiersCount();
+
+	// Get All Courses
+	getAllCourses();
+
+	// Get All Soldiers
+	getAllSoldiers();
+
+	// Get Overall Average Score for each Soldier
+	OpGetOverallAvg("Test","TEST_","overallAvgTable");
+
+}
+
+function OpUpdateStatistics(){
+
+	// Get All Soldiers Count
+	OpGetCourseCount();
+
+	// Get All Soldiers Count
+	OpGetSoldiersCount();
+
+	// Get All Courses
+	OpGetAllCourses();
+
+	// Get All Soldiers
+	OpGetAllSoldiers();
+
+	// Get Overall Average Score for each Soldier
+	OpGetOverallAvg("","","OpOverallAvgTable");
+	//OpGetOverallAvg();
+
+}
+
+
+function createRandomData(){
+	var CourseCount = $("#courseCount").val();
+	var SoldierCount = $("#soldierCount").val();
+	console.log(CourseCount );
+	console.log(SoldierCount );
+	checkDatabase(CourseCount, SoldierCount);
+}
+
+function emptyDatabase(){
+
+	// Cypher query to delete all the data in Neo4j
+	var query="MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r";
+
+	session.run(query).then( function (result) {
+
+		var time = result.summary.resultAvailableAfter.low + result.summary.resultConsumedAfter.low;
+		alert("הנתונים נמחקו ב- "+time+" מילי-שניות");
+		updateStatistics();
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+
+//Function Call To Display Test Data Statistics at initial load
+updateStatistics();
+
+//Function Call To Display Test Data Statistics at initial load
+OpUpdateStatistics();
+
 //========================================================================================
 window.addEventListener("DOMContentLoaded", function(e) {
 
---
+++ Utilities.js
@@ -0,0 +1,261 @@
+// Function to get soldier data based on filter
+function getSoldierDataForCourses(id,element_id,nodeLabel,relLabel){
+	var response=[];
+			
+	// Construct a Neo4j Query to get grades of a soldier
+	var query = 'MATCH (s:'+nodeLabel+'Soldier{id:'+id+'})-[:'+relLabel+'RECEIVED]->(g:'+nodeLabel+'Grade) RETURN g.course as course,avg(g.grade) as avg_grade';
+
+	// Run query using session
+	session.run(query).then( function (result) {
+		// Close the neo4j session
+		result.records.forEach(function (record) {
+			//console.log( record.get('name'), ":", record.get('grade'),":", record.get('course') );
+			var obj={};
+			obj['course']=record.get('course')
+			obj['avg_grade']=Math.round(record.get('avg_grade') * 100) / 100;
+			response.push(obj);
+            var time = result.summary.resultAvailableAfter.low + result.summary.resultConsumedAfter.low;
+            alert("הנתונים נבדקו ב- "+time+" מילי-שניות");
+		});
+
+		// Empty previous table
+		document.getElementById(element_id).innerHTML=""
+
+		// Check if result has records then call to draw table function else alert no data
+		if(response.length > 0){
+			drawCourseAvgForSoldierTable(response,element_id);
+		}else{
+			// Empty the output table
+			//document.getElementById("outputTable").innerHTML=""
+			// Please write your own alert here.
+			alert('לא קיימים נתונים עבור חייל זה!');
+		}
+		console.log("SUCCESS");
+
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+// Function to draw output table
+function drawCourseAvgForSoldierTable(data,element_id) {
+	// Define Header for table graded_by
+	header={
+		'course':'מספר קורס',
+		'avg_grade':'ציון ממוצע'
+	}
+	// Call a function to draw row
+	drawCourseAvgForSoldierRow(header,element_id)
+	for (var i = 0; i < data.length; i++) {
+        drawCourseAvgForSoldierRow(data[i],element_id);
+    }
+	$("#"+element_id).addClass("table");
+
+}
+
+//Function to draw row
+function drawCourseAvgForSoldierRow(rowData,element_id) {
+    var row = $("<tr />")
+    $("#"+element_id).append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+    row.append($("<td>" + rowData.course+ "</td>"));
+    row.append($("<td>" + rowData.avg_grade + "</td>"));
+}
+
+
+// Function to get soldier data based on filter
+function getCourseDataForSoldiers(id,html_element,nodeLabel,relLabel){
+	var response=[];
+
+	// Construct a Neo4j Query to get grades of a soldier
+	var query = 'MATCH (s:'+nodeLabel+'Soldier)-[:'+relLabel+'RECEIVED]->(g:'+nodeLabel+'Grade{course:'+id+'}) RETURN s.id as soldier_id,avg(g.grade) as avg_grade';
+	// Run query using session
+	session.run(query).then( function (result) {
+		// Close the neo4j session
+		result.records.forEach(function (record) {
+			//console.log( record.get('name'), ":", record.get('grade'),":", record.get('course') );
+			var obj={};
+			obj['soldier_id']=record.get('soldier_id')
+			obj['avg_grade']=Math.round(record.get('avg_grade') * 100) / 100;
+			response.push(obj);
+       });
+        var time = result.summary.resultAvailableAfter.low + result.summary.resultConsumedAfter.low;
+        alert("הנתונים נבדקו ב- "+time+" מילי-שניות");
+
+        // Empty previous table
+		document.getElementById(html_element).innerHTML=""
+
+		// Check if result has records then call to draw table function else alert no data
+		if(response.length > 0){
+			drawAvgForCourseTable(response,html_element);
+		}else{
+			// Please write your own alert here.
+			alert('לא קיימים נתונים עבור קורס זה!');
+		}
+		//console.log("SUCCESS");
+
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+// Function to draw output table
+function drawAvgForCourseTable(data,html_element) {
+	// Define Header for table graded_by
+	header={
+		'soldier_id':'מספר אישי',
+		'avg_grade':'ציון ממוצע'
+	}
+	// Call a function to draw row
+	drawAvgForCourseRow(header,html_element)
+	for (var i = 0; i < data.length; i++) {
+        drawAvgForCourseRow(data[i],html_element);
+    }
+	$("#"+html_element).addClass("table");
+
+}
+
+// Function to draw row
+function drawAvgForCourseRow(rowData,html_element) {
+    var row = $("<tr />")
+    $("#"+html_element).append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+    row.append($("<td>" + rowData.soldier_id+ "</td>"));
+    row.append($("<td>" + rowData.avg_grade + "</td>"));
+}
+
+
+
+
+//Create a function to get overall average for each soldier
+function OpGetOverallAvg(nodeLabel,RelLabel,id){
+	var response=[];
+	var query = 'MATCH (s:'+nodeLabel+'Soldier)-[:'+RelLabel+'RECEIVED]-(g:'+nodeLabel+'Grade) return s.id as soldier_id, avg(g.grade) as avg_grade';
+	session.run(query).then( function (result) {
+
+		// Empty previous table
+		$("#"+id).html('');
+		console.log("With New Desing");
+		// Iterate over all the records
+		result.records.forEach(function (record) {
+			var obj={};
+			obj['soldier_id']=record.get('soldier_id');
+			obj['avg_grade']=Math.round(record.get('avg_grade') * 100) / 100;
+			response.push(obj);
+		});
+
+		// Check if result has records then call to draw table function else alert no data
+		if(response.length > 0){
+			drawAvgTable(response,id);
+		}else{
+			// Empty the output table
+			//document.getElementById("outputTable").innerHTML=""
+			// Please write your own alert here.
+			//alert('No data available for the applied filters!');
+			console.log('לא קיימים נתונים לפי סינון זה!');
+		}
+		//console.log("SUCCESS");
+        //var time = result.summary.resultAvailableAfter.low + result.summary.resultConsumedAfter.low;
+       // alert("הנתונים נבדקו ב- "+time+" מילי-שניות");
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+// Function to draw output table
+function drawAvgTable(data,id) {
+	// Define Header for table graded_by
+	header={
+		'soldier_id':'מספר אישי',
+		'avg_grade':'ציון ממוצע'
+	}
+	// Call a function to draw row
+	drawAvgRow(header,id)
+	for (var i = 0; i < data.length; i++) {
+		drawAvgRow(data[i],id);
+    }
+	$("#"+id).addClass("table");
+
+}
+
+// Function to draw row
+function drawAvgRow(rowData,id) {
+    var row = $("<tr />")
+    $("#"+id).append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+    row.append($("<td>" + rowData.soldier_id + "</td>"));
+    row.append($("<td>" + rowData.avg_grade + "</td>"));
+}
+
+
+function checkDatabase(courseCount,soldierCount){
+	var queryTime={};
+	
+	/***** CREATE RANDOM SOLDIERS ******/ 
+	var query = 'FOREACH (r IN range(1,'+soldierCount+') | CREATE (:TestSoldier {id:r}))';
+
+	// Run query using session
+	session.run(query).then( function (result1) {
+		
+		// store query time required to add soldiers
+		queryTime['Add Soldiers']= result1.summary.resultAvailableAfter.low + result1.summary.resultConsumedAfter.low;
+		console.log('Time taken to add Soldiers: '+queryTime['Add Soldiers']);
+
+		/***** CREATE RANDOM Courses ******/ 
+		query = 'FOREACH (r IN range(1,'+courseCount+') | CREATE (:TestCourse {id:r}))';
+		session.run(query).then( function (result2) {
+			
+			// store query time required to add courses
+			queryTime['Add Courses']= result2.summary.resultAvailableAfter.low + result2.summary.resultConsumedAfter.low;
+			console.log('Time taken to add Courses: '+queryTime['Add Courses']);
+			
+			/***** CREATE RANDOM GRADES ******/
+			var gradetime = 0;
+			for(var i=0; i <=soldierCount; i++ ){ 
+				query = 'MATCH (c:TestCourse{id:toInt('+courseCount+'*rand())}),(s1:TestSoldier {id:'+i+'}),(s2:TestSoldier {id:toInt('+soldierCount+'*rand())})'
+					+ ' WITH s1,s2,c'
+					+ ' LIMIT '
+					+ soldierCount
+					+ ' WHERE rand() < 0.5 AND s1.id <> s2.id'
+					+ ' CREATE (g:TestGrade{id:s1.id+\'_\'+s2.id+\'_\'+c.id, course:c.id, grade: 1+toInt(10*rand())})'
+					+ ' MERGE (s2)-[:TEST_OPTED]->(c)'
+					+ ' CREATE (g)-[:TEST_FOR]->(c)'
+					+ ' CREATE (s2)-[:TEST_RECEIVED]->(g)'
+					+ ' CREATE (s1)-[:TEST_GIVEN]->(g)';
+				session.run(query).then( function (result3) {
+					
+					// store query time required to add Grades
+					gradetime += (result3.summary.resultAvailableAfter.low + result3.summary.resultConsumedAfter.low);
+	
+				}).catch(function (error) {
+					console.log(error);
+				});
+			}	
+			queryTime['Add Grades'] = gradetime;
+			console.log('Time taken to add Grades: '+queryTime['Add Grades']);
+			
+			// Update required time
+			setTime(queryTime);
+			updateStatistics();
+
+			}).catch(function (error) {
+				console.log(error);
+			});
+	}).catch(function (error) {
+		console.log(error);
+	});
+	
+}
+
+function setTime(queryTime){
+	var totalTime=0;
+	for( var i in queryTime){
+		totalTime += queryTime[i];
+	}
+	//console.log(queryTime);
+	console.log("Total Time: "+totalTime);
+
+	$("#performanceResult").addClass("table");
+    var row = $("<tr />")
+    $("#performanceResult").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+    row.append($("<td> זמן כולל במילי-שניות :</td>"));
+    row.append($("<td>" + totalTime + "</td>"));
+}
+	
